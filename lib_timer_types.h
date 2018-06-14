09d2077ceb0cecb89f7eafcf72b25f722d609ab3
lp_thomas <t.willetal@gmail.com>
2018-06-14 06:38:50 +0200

    add to a timer types header

---
+++ lib_timer_types.h
@@ -0,0 +1,50 @@
+/* ****************************************************************************************************
+ * lib_timer_types.h within the following project: bld_device_cmake_LINUX
+ *	
+ *  compiler:   GNU Tools ARM Embedded (4.7.201xqx)
+ *  target:     Cortex Mx
+ *  author:		thomas
+ * ****************************************************************************************************/
+
+/* ****************************************************************************************************/
+
+/*
+ *	******************************* change log *******************************
+ *  date			user			comment
+ * 	19.04.2018			thomas			- creation of lib_timer.h
+ *  
+ */
+
+
+
+#ifndef LIB_TIMER_SH_LIB_TIMER_STM32_LIB_TIMER_TYPES_H_
+#define LIB_TIMER_SH_LIB_TIMER_STM32_LIB_TIMER_TYPES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* *******************************************************************
+ * includes
+ * ******************************************************************/
+
+/* *******************************************************************
+ * defines
+ * ******************************************************************/
+
+/* *******************************************************************
+ * custom data types (e.g. enumerations, structures, unions)
+ * ******************************************************************/
+enum timer_mode {
+	TIMER_MODE_callback,
+	TIMER_MODE_cyclic
+};
+
+typedef struct internal_timer* timer_hdl_t;
+typedef void (timer_cb_t)(timer_hdl_t _hdl, void* _arg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INT_OS_BASIC_LIB_TIMER_SH_LIB_TIMER_STM32_LIB_TIMER_H_ */
